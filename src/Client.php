993da187fa8e35c5722d45a311423741e4891216
bagusrin <bagusrinn@gmail.com>
2018-04-01 23:19:46 +0700

    first commit

---
+++ src/Client.php
@@ -0,0 +1,36 @@
+<?php
+namespace Bagurin\Twitter;
+
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+class Client
+{
+    private $client;
+    public function __construct(TwitterOAuth $client)
+    {
+        $this->client = $client;
+    }
+    public function getClient(): TwitterOAuth
+    {
+        return $this->client;
+    }
+    public function getTimeline(int $count = 25): array
+    {
+        $statuses = $this->client->get('statuses/home_timeline', ['count' => $count]);
+        return $statuses;
+    }
+    public function postStatus(string $message, array $mediaPaths = [])
+    {
+        $media_ids = [];
+        foreach ($mediaPaths as $mediaPath) {
+            $media = $this->client->upload('media/upload', ['media' => $mediaPath]);
+            $media_ids[] = $media->media_id_string;
+        }
+        $parameters = [
+            'status' => $message,
+            'media_ids' => implode(',', $media_ids),
+        ];
+        $response = $this->client->post('statuses/update', $parameters);
+        return $response;
+    }
+}
---
+++ tests/ClientTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace Bagusrin\Twitter\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ClientTest extends TestCase
+{
+    public function testNoTestsYet(): void
+    {
+        $this->assertTrue(true);
+    }
+}
