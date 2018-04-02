113165a585d903c96ba150d46dcf67c2838f4fc3
bagusrin <bagusrinn@gmail.com>
2018-04-02 12:20:14 +0700

    update client

--- src/Client.php
+++ src/Client.php
@@ -1,36 +1,50 @@
 <?php
-namespace Bagurin\Twitter;
+namespace Bagusrin\Twitter;
 
 use Abraham\TwitterOAuth\TwitterOAuth;
 
-class Client
-{
+class Client{
+
     private $client;
-    public function __construct(TwitterOAuth $client)
-    {
+    
+    public function __construct(TwitterOAuth $client){
         $this->client = $client;
     }
-    public function getClient(): TwitterOAuth
-    {
+
+    public function getClient(){
         return $this->client;
     }
-    public function getTimeline(int $count = 25): array
-    {
-        $statuses = $this->client->get('statuses/home_timeline', ['count' => $count]);
-        return $statuses;
+
+    public function getTimeline(int $count = 25){
+        
+        $param = ['count' => $count];
+        
+        $ret = $this->client->get('statuses/home_timeline', $param);
+        return $ret;
     }
-    public function postStatus(string $message, array $mediaPaths = [])
-    {
+
+    public function searchTweets(string $search, string $type, int $count = 25){
+        
+        $param = ['q' => $search, 'result_type' => $type, 'count' => $count, 'tweet_mode' => 'extended'];
+        
+        $ret = $this->client->get('search/tweets', $param);
+        return $ret;
+    }
+
+    public function postStatus(string $message, array $mediaPaths = []){
+        
         $media_ids = [];
         foreach ($mediaPaths as $mediaPath) {
             $media = $this->client->upload('media/upload', ['media' => $mediaPath]);
             $media_ids[] = $media->media_id_string;
         }
+
         $parameters = [
             'status' => $message,
             'media_ids' => implode(',', $media_ids),
         ];
-        $response = $this->client->post('statuses/update', $parameters);
-        return $response;
+
+        $ret = $this->client->post('statuses/update', $parameters);
+        return $ret;
     }
 }
